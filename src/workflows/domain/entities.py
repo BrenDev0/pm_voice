1a977470751aeae7fbf8a064225e89fcdf4f54e6
BrenDev0 <lahey1991@gmail.com>
2025-10-17 14:04:47 -0600

    stri for datetime

--- src/workflows/domain/entities.py
+++ src/workflows/domain/entities.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class Event(BaseModel):
     title: str
-    appoinment_datetime: datetime
+    appoinment_datetime: str
     description: Optional[str] = None
     attendees: List[str]
 
